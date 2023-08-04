f8c7c2b8bced327a50a0b250258e6c77a0107a85
Tadai <tadai@jota.one>
2023-08-04 18:01:44 +0200

    feat: switch font to Inter + cleanup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,27 @@
 // https://nuxt.com/docs/guide/directory-structure/nuxt.config#nuxt-config-file
+import webfontDownload from 'vite-plugin-webfont-dl'
+
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.bunny.net/css?family=inter:100,200,300,400,500,600,700,800,900)'
+        }
+      ]
+    }
+  },
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   content: {
     documentDriven: true
   },
   css: ['@/assets/styles/main.pcss'],
+  vite: {
+    plugins: [
+      webfontDownload([
+        'https://fonts.bunny.net/css?family=inter:100,200,300,400,500,600,700,800,900)',
+      ])
+    ]
+  }
 })
